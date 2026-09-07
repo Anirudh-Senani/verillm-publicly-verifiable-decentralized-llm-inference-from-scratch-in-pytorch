127006017cf943b1744cdf202e3f100d699f5489
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-09-07 09:36:00 +0530

    Step 7: compute_attention_scores

--- model.py
+++ model.py
@@ -70,8 +70,10 @@ def linear_projection(x, weight, bias=None):
 
     return out
 
-# Step 7 - compute_attention_scores (not yet solved)
-# TODO: implement
+# Step 7 - compute_attention_scores
+def compute_attention_scores(queries, keys):
+    # TODO: return the (Tq, Tk) matrix of raw dot-product scores between queries and keys.
+    return queries @ keys.T
 
 # Step 8 - scale_attention_scores (not yet solved)
 # TODO: implement
