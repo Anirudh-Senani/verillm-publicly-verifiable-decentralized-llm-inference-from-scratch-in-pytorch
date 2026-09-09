e3a3bf566d6e35add066a72398bdd648658097c9
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-09-09 14:45:48 +0530

    Step 49: sample_verifier_committee

--- model.py
+++ model.py
@@ -580,8 +580,12 @@ def show_tampered_transcript_rejected(transcript, model_params, position, new_to
         rejected=not result['accept']
     )
 
-# Step 49 - sample_verifier_committee (not yet solved)
-# TODO: implement
+# Step 49 - sample_verifier_committee
+import random
+
+def sample_verifier_committee(verifier_ids, committee_size, seed):
+    # TODO: deterministically sample committee_size distinct ids from verifier_ids using seed.
+    return random.Random(seed).sample(verifier_ids, committee_size)
 
 # Step 50 - collect_verifier_votes (not yet solved)
 # TODO: implement
